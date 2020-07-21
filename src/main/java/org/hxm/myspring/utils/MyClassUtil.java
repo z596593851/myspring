8884283f9bbd9996881c4a42741b92e5663d93d5
huxiaoming02 <huxiaoming02@58.com>
2020-07-22 01:06:49 +0800

    基本的注解注入流程完成，下一步解析compoment

---
+++ src/main/java/org/hxm/myspring/utils/MyClassUtil.java
@@ -0,0 +1,35 @@
+package org.hxm.myspring.utils;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.ClassUtils;
+
+public class MyClassUtil {
+
+    public static Class<?> forName(String name, ClassLoader classLoader) throws Exception{
+        return Class.forName(name, false, classLoader);
+    }
+
+    public static ClassLoader getDefaultClassLoader() {
+        ClassLoader cl = null;
+        try {
+            cl = Thread.currentThread().getContextClassLoader();
+        }
+        catch (Throwable ex) {
+            // Cannot access thread context ClassLoader - falling back...
+        }
+        if (cl == null) {
+            // No thread context class loader -> use class loader of this class.
+            cl = MyClassUtil.class.getClassLoader();
+            if (cl == null) {
+                // getClassLoader() returning null indicates the bootstrap ClassLoader
+                try {
+                    cl = ClassLoader.getSystemClassLoader();
+                }
+                catch (Throwable ex) {
+                    // Cannot access system ClassLoader - oh well, maybe the caller can live with null...
+                }
+            }
+        }
+        return cl;
+    }
+}
